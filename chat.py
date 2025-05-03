4207114855e52c8156c0aa516818f0dacf0f5c54
atharvakaplay123 <atharvakaplay@gmail.com>
2025-05-03 10:11:21 +0530

    Firebase added

--- chat.py
+++ chat.py
@@ -2,13 +2,36 @@
 
 from google import genai
 from google.genai import types
-from secret import API_KEY
+import secret
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
 
-client = genai.Client(api_key = API_KEY)
+# Path to your downloaded service account key JSON file
+cred = credentials.Certificate(secret.DB_JSON)
+
+# Initialize the app with a service account, granting admin privileges
+firebase_admin.initialize_app(cred, {
+    'databaseURL': secret.DB_URL
+})
+ref = db.reference('sensorData')
+
+# Example: Writing to the database
+# ref.set({
+#     'temperature': 25,
+#     'humidity': 60
+# })
+
+# Example: Reading from the database
+data = ref.get()
+print("Fetched data:", data)
+
+Instruction = "Your name is Sonu, you are developed by Atharva Kaplay. Here is the data provided by the sensor node from the feild " + str(data)
+client = genai.Client(api_key = secret.API_KEY)
 history=[
     types.Content(
             role='user',
-            parts=[types.Part.from_text(text="Your name is Sonu")]
+            parts=[types.Part.from_text(text=Instruction)]
         )
 ]
 while True:
